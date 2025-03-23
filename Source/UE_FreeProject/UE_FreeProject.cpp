77485252885a80de67e3675845dd3be6a748cfb7
HIGEON <rla5233@naver.com>
2025-03-23 22:44:48 +0900

    프로젝트 생성

---
+++ Source/UE_FreeProject/UE_FreeProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_FreeProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_FreeProject, "UE_FreeProject" );
