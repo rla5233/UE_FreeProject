2f53f2357d380db405f1f0ae65e872e350f2affd
HIGEON <rla5233@naver.com>
2025-04-08 23:11:52 +0900

    스파인 플러그인 설치

--- Source/UE_FreeProject/UE_FreeProject.Build.cs
+++ Source/UE_FreeProject/UE_FreeProject.Build.cs
@@ -8,7 +8,7 @@ public class UE_FreeProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "SpinePlugin" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
